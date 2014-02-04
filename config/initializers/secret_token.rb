f6b43b0f12c55bb9ba1e32741134d98a31761628
Cody Poll <CJPoll@gmail.com>
2014-02-03 23:29:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sra::Application.config.secret_token = '6a5588634aac67edd04aace6f67b56b70c8b8c787fc57e1503e55751e7f4d1a342a3621b4f2b78263123483fe080a5a68c1ceba49d4fb39d2866f2d779624f44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sra::Application.config.session_store :cookie_store, key: '_sra_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sra::Application.config.session_store :active_record_store
