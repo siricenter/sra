359938bebccfe24f6e040c806b7b72a212406b27
Cody Poll <CJPoll@gmail.com>
2014-02-04 23:15:11 -0700

    PersonController is working for all main operations.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,24 +1,47 @@
 class PeopleController < ApplicationController
-  def new
-	  @person = Person.new
-	  @edu_options = ['None', 'Some Elementary', 'Elementary', 'Some Secondary', 'Secondary', 'Some College', 'College']
-  end
+	def new
+		@person = Person.new
+	end
 
-  def create
-  end
+	def create
+		@person = Person.new(params[:person])
 
-  def edit
-  end
+		respond_to do |format|
+			if @person.save
+				format.html {redirect_to @person }
+			else
+				format.html {render action: :new }
+			end
+		end
+	end
 
-  def update
-  end
+	def edit
+		@person = Person.find(params[:id])
+	end
 
-  def index
-  end
+	def update
+		@person = Person.new(params[:person])
+		
+		respond_to do |format|
+			if @person.update_attributes(params[:thing])
+				format.html {render action: 'show'}
+			end
+		end
+	end
 
-  def show
-  end
+	def index
+		@people = Person.all
+	end
 
-  def destroy
-  end
+	def show
+		@person = Person.find(params[:id])
+	end
+
+	def destroy
+		@person = Person.find(params[:id])
+
+		if @person.destroy
+			redirect_to people_path
+		end
+	end
 end
