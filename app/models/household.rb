88fc10ff3a6dfd69ce13cbbb2c745c99d2b886bc
Cody Poll <cjpoll@gmail.com>
2014-02-10 17:21:19 -0500

    Generated annotations for existing model objects

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,6 +1,16 @@
+# == Schema Information
+#
+# Table name: households
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Household < ActiveRecord::Base
   attr_accessible :name
   has_many :people
-  
+
   accepts_nested_attributes_for :people
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id              :integer          not null, primary key
+#  given_name      :string(255)
+#  family_name     :string(255)
+#  birthday        :date
+#  education_level :string(255)
+#  gender          :string(255)
+#  in_school       :boolean
+#  is_alive        :boolean
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  household_id    :integer
+#
+
 class Person < ActiveRecord::Base
   attr_accessible :birthday, :education_level, :education_level, :family_name, :gender, :given_name, :in_school, :is_alive
   belongs_to :household
