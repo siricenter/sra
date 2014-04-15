93081b584f4ced77ee6519c4d06f585190d17f72
Cody Poll <CJPoll@gmail.com>
2014-04-15 13:39:42 -0600

    Basic interview stuff is working

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -1,2 +1,16 @@
 module InterviewsHelper
+	def selection name, options, name_hash = "interview"
+		select_string = "<select name=\"#{name_hash}[#{name}]\">\n"
+		select_string += options(options)
+		select_string += "</select>"
+		select_string.html_safe
+	end
+
+	def options options
+		option_string = ""
+		options.each do |option|
+			option_string += "<option value=#{option}>#{option.capitalize}</option>\n"
+		end
+		return option_string.html_safe
+	end
 end
