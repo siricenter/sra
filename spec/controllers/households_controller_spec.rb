0f7a4e4f79a62d2cf3b735da72e88d0ed4555955
CJPoll <CJPoll@gmail.com>
2014-05-05 15:57:39 -0600

    Added stubbed HouseholdsController Spec

---
+++ spec/controllers/households_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+describe HouseholdsController do
+	let(:valid_attributes) {{}}
+	describe "GET index"
+	describe "GET show" 
+	describe "GET new"
+	describe "GET edit"
+	describe "POST create"
+	describe "PUT update"
+end
