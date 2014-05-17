38b7fb90124455bd7521b526583c11df1b90a91a
CJPoll <CJPoll@gmail.com>
2014-05-16 18:19:22 -0600

    Fixed bug that prevented role assignment

--- app/controllers/user_role_controller.rb
+++ app/controllers/user_role_controller.rb
@@ -1,14 +1,21 @@
 class UserRoleController < ApplicationController
+	#before_filter :authenticate_user!
+
 	def create
 		@user = User.find(params[:user_id])
 		@role = Role.find(params[:role_id])
 
-		@user.roles << @role unless @user.roles.find_by_name(@role.name)
-		respond_to do |format|
-			if @user.save!
-				format.html {redirect_to user_path(@user), notice: "Role successfully assigned"}
-			else
-				format.html {redirect_to user_path(@user), alert: "Saving failed"}
+		unless can? :create, :user_role
+			redirect_to @user, alert: "You don't have rights to assign roles"
+		else
+
+			@user.roles << @role unless @user.roles.find_by_name(@role.name)
+			respond_to do |format|
+				if @user.save!
+					format.html {redirect_to user_path(@user), notice: "Role successfully assigned"}
+				else
+					format.html {redirect_to user_path(@user), alert: "Saving failed"}
+				end
 			end
 		end
 	end
