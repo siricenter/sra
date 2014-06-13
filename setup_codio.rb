825e87b624e12f8467e960860ad19f5e490ab993
CJPoll <cjpoll@gmail.com>
2014-06-13 03:51:41 +0000

    Still working on setup_codio.rb

--- setup_codio.rb
+++ setup_codio.rb
@@ -1,13 +1,32 @@
+#!/usr/bin/env ruby
 def bundle_install
-	`cd ~/workspace`
-	`bundle install`
+	puts `cd ~/workspace`
+	puts `bundle install`
 end
 
 def install_mysql
-	`parts install mariadb`
-	`parts start mariadb`
+    puts `parts install mariadb`
+    puts `parts stop mariadb`
+	puts `parts start mariadb`
 end
 
-install_mysql
-install_mongo
-bundle_install
+def install_mongo
+	puts `parts install mongodb`
+    puts `parts stop mongodb`
+    puts `parts start mongodb`
+end
+
+def setup_database
+    puts `mysql -u root -p < setup_database.sh`
+end
+
+def start_server
+    puts `cd ~/workspace`
+    puts `rails server -d`
+end
+
+#install_mysql
+#install_mongo
+#bundle_install
+setup_database
+start_server
