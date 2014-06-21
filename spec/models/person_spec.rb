49fa946966b05072a2632bc7782b801f230722cd
CJPoll <cjpoll@gmail.com>
2014-06-21 02:47:39 +0000

    person_spec was annotated with the database schema comments.

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id                     :integer          not null, primary key
+#  given_name             :string(255)
+#  family_name            :string(255)
+#  family_relationship_id :integer
+#  birthday               :date
+#  education_level        :string(255)
+#  gender                 :string(255)
+#  in_school              :boolean
+#  is_alive               :boolean
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  household_id           :integer
+#
+
 require 'spec_helper'
 
 describe Person do
