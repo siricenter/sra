988559f0fd9a123d10082678ea9d55e87506f37f
CJPoll <cjpoll@gmail.com>
2014-07-02 19:50:54 +0000

    Nutrition page shows calories, sugar, cholesterol, protein, and sodium per person

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -13,4 +13,14 @@ module InterviewsHelper
 		end
 		return option_string.html_safe
 	end
+
+	def nutrient_row label, amount, people
+		row = <<-EOH
+			<div class="row">
+				<input disabled="true", value="#{amount / people}", class="textfield col-sm-1"/>
+				<label class="col-sm-11">#{label} Per Person</label>
+			</div>
+		EOH
+		row.html_safe
+	end
 end
