7c35767275272bc83428d42caf74180f0aae6089
Cody Poll <CJPoll@gmail.com>
2014-08-01 11:25:49 -0600

    Dashboard is context specific for field workers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,13 @@
 class DashboardController < ApplicationController
-  def show
-	  @households = Household.all
-  end
+	def show
+		@user=current_user
+		if @user.has_role? "Field Worker"
+			field_worker
+		end
+	end
+
+	def field_worker
+		@households = Household.all
+		render :field_worker
+	end
 end
