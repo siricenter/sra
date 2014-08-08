100158cb02aa5693de77d93201d858ea6be5fb89
Cody Poll <CJPoll@gmail.com>
2014-08-08 02:03:47 -0600

    Some nasty code style got fixed.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,37 +8,23 @@ class DashboardController < ApplicationController
         elsif @user.has_role? "manager"
             manager
         elsif @user.has_role? "field worker"
-            
             field_worker
         elsif @user.has_role? "public"
             redirect_to households_path
         end
-       
-      
 	end
 
 	def field_worker
-        @households=@user.households
-        
-        
-        
+        @households = @user.households
         render :worker
-        
-		
 	end
     
     def admin
         @user = User.new
-        @user
-            
-            
         render :admin
-        
     end
     
     def manager
         render :manager
-        
     end
-    
 end
