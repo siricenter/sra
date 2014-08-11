51bf548965c830a022b5e1a31135065528f07278
Cody Poll <CJPoll@gmail.com>
2014-08-10 19:25:37 -0600

    Moved user helper for role panel over to a partial.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,2 @@
 module UsersHelper
-	def role_panel role, assign = true
-		panel = <<-EOH
-		<div class="col-sm-6 col-md-4">
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<h3 class="panel-title">#{role.name}</h3>
-				</div>
-				<div class="panel-body index">
-		#{ if assign then link_to 'Assign', new_user_role_path(@user.id, role.id), class: [:btn, "btn-success", "btn-lg", "col-sm-12"], id: "#{role.id}_assign", method: :post end}
-		#{ unless assign or role.name == "public" then link_to 'Unassign', destroy_user_role_path(@user.id, role.id), class: [:btn, "btn-success", "btn-lg", "col-sm-12"], id: "#{role.id}_unassign", method: :delete end}
-				</div>
-			</div>
-		</div>
-		EOH
-		panel.html_safe
-	end
 end
