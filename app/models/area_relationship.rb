45fb82b99a60995b5f671ea5d203d3c0f0ce7c2f
Cody Poll <CJPoll@gmail.com>
2014-08-12 20:58:14 -0600

    Ran annotate. AreaRelationship now annotated.

--- app/models/area_relationship.rb
+++ app/models/area_relationship.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: area_relationships
+#
+#  id           :integer          not null, primary key
+#  area_id      :integer
+#  user_id      :integer
+#  relationship :string(255)
+#
+
 class AreaRelationship < ActiveRecord::Base
     belongs_to :area
     belongs_to :user
