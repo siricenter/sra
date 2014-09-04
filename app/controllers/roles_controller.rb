9abf7ab337185179770910022c0391542c4b2c29
andrewf2 <andrewf02@gmail.com>
2014-09-04 04:38:49 +0000

    made roles controller ready to interact with API instead activerecord

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -23,8 +23,9 @@ class RolesController < ApplicationController
 	end
 
 	def edit
-		@role = Role.find(params[:id])
-
+		#@role = Role.find(params[:id])
+		request = RestClient.get 'https://sra-api.com/roles/:id' {:params => {:id => params[:id]}}
+        @role = JSON.parse(request)
 		respond_to do |format|
 			format.html # new.html.erb
 			format.json { render json: @role }
@@ -32,14 +33,17 @@ class RolesController < ApplicationController
 	end
 
 	def new
-		@role = Role.new
+		#@role = Role.new
+        RestClient.post 'https://sra-api.com/roles/new' 
 	end
 
 	def create
-		@role = Role.new(params[:role])
-
+		#@role = Role.new(params[:role])
+        RestClient.post 'https://sra-api.com/roles', {:params => {:role => params[:role]}}
+        request = RestClient.get 'https://sra-api.com/roles/:id' {:params => {:id => params[:id]}}
+        @role = JSON.parse(request)
 		respond_to do |format|
-			if @role.save
+            if @role
 				format.html { redirect_to roles_path, notice: "Role successfully created" }
 				format.json {render json: @role, status: :created, location: role_path(@role)}
 			else
@@ -50,10 +54,12 @@ class RolesController < ApplicationController
 	end
 
 	def update
-		@role = Role.find(params[:id])
-
+		#@role = Role.find(params[:id])
+		request = RestClient.get 'https://sra-api.com/roles/:id' {:params => {:id => params[:id]}}
+        @role = JSON.parse(request)
+        RestClient.put 'https://sra-api.com/roles/:id' {:params => {@role => params[:role]}}
 		respond_to do |format|
-			if @role.update_attributes(params[:role])
+			if @role
 				format.html { redirect_to roles_path, notice: "Role was successfully updated" }
 				format.json { head :no_content }
 			else
@@ -64,8 +70,10 @@ class RolesController < ApplicationController
 	end
 
 	def destroy
-		@role = Role.find(params[:id])
-		@role.destroy
+		#@role = Role.find(params[:id])
+        request = RestClient.get 'https://sra-api.com/roles/:id' {:params => {:id => params[:id]}}
+        @role = JSON.parse(request)
+        RestClient.delete 'https://sra-api.com/roles/:id' {:params => {:id => params[:id]}}
 
 		respond_to do |format|
 			format.html { redirect_to roles_path }
