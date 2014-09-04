215956815b8c390ab9dd780a19aa2a3727c85c14
andrewf2 <andrewf02@gmail.com>
2014-09-04 18:03:50 +0000

    user_role controller is now implementing rest-client

--- app/controllers/user_role_controller.rb
+++ app/controllers/user_role_controller.rb
@@ -2,8 +2,13 @@ class UserRoleController < ApplicationController
 	#before_filter :authenticate_user!
 
 	def create
-		@user = User.find(params[:user_id])
-		@role = Role.find(params[:role_id])
+		#@user = User.find(params[:user_id])
+        request = RestClient.get 'https://sra-api.com/users/:id', {:params => {:id => params[:user_id]}}
+        @user = JSON.parse(request)
+        
+		#@role = Role.find(params[:role_id])
+        request = RestClient.get 'https://sra-api.com/roles/:id', {:params => {:id => params[:role_id]}}
+        @role = JSON.parse(request)
 
 		unless can? :create, :user_role
 			redirect_to @user, alert: "You don't have rights to assign roles"
@@ -22,9 +27,12 @@ class UserRoleController < ApplicationController
 
 	def destroy
 		if can? :delete, :user_role
-			@user = User.find(params[:user_id])
-			@role = Role.find(params[:role_id])
-
+			#@user = User.find(params[:user_id])
+            request = RestClient.get 'https://sra-api.com/users/:id', {:params => {:id => params[:user_id]}}
+       		@user = JSON.parse(request)
+			#@role = Role.find(params[:role_id])
+            request = RestClient.get 'https://sra-api.com/roles/:id', {:params => {:id => params[:role_id]}}
+        	@role = JSON.parse(request)
 			@user.roles.delete(@role) if @user.roles.include?(@role)
 		end
 
