3fabef43fea957db278637eaa276409bcdeda67d
andrewf2 <andrewf02@gmail.com>
2014-09-04 23:31:09 +0000

    Events controller now has rest client implemented

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,8 +3,9 @@ class EventsController < ApplicationController
 	# GET /events
 	# GET /events.json
 	def index
-		@events = current_user.events
-
+		#@events = current_user.events
+        request = RestClient.get 'https://sra-api.com/users/:id/events', {:params => {:id => params[:user_id]}}
+        @events = JSON.parse(requests)
 		respond_to do |format|
 			format.html # index.html.erb
 			format.json { render json: @events }
@@ -14,8 +15,9 @@ class EventsController < ApplicationController
 	# GET /events/1
 	# GET /events/1.json
 	def show
-		@event = Event.find(params[:id])
-
+		#@event = Event.find(params[:id])
+        request = RestClient.get 'https://sra-api.com/users/events/:id', {:params => {:id => params[:id]}} 
+        @event = JSON.parse(request)
 		unless @event.user == current_user
 			raise CanCan::AccessDenied
 		end
@@ -30,7 +32,9 @@ class EventsController < ApplicationController
 	# GET /events/new.json
 	def new
 		@event = Event.new
-
+        RestClient.post 'https://sra-api.com/users/events'
+        request = RestClient.get 'https://sra-api.com/users/events/last'
+        @event = JSON.parse(json)
 		respond_to do |format|
 			format.html # new.html.erb
 			format.json { render json: @event }
@@ -39,19 +43,26 @@ class EventsController < ApplicationController
 
 	# GET /events/1/edit
 	def edit
-		@event = Event.find(params[:id])
+		#@event = Event.find(params[:id])
+        request = RestClient.get 'https://sra-api.com/users/events/:id', {:params => {:id => params[:id]}} 
+        @event = JSON.parse(request)
 	end
 
 	# POST /events
 	# POST /events.json
 	def create
-		user = current_user
-		@event = Event.new(params[:event])
-
+		#user = current_user
+        request = RestClient.get 'https//sra-api.com/users/current' {:accept => :json}
+        user = JSON.parse(request)
+		#@event = Event.new(params[:event])
+        request = RestClient.get 'https://sra-api.com/users/events/:event', {:params => {:event => params[:event]}} 
+        @event = JSON.parse(request)
 		user.events << @event
-
+        RestClient.put 'https://sra-api.com/users/current'
+        request = RestClient.get 'https//sra-api.com/users/current' {:accept => :json}
+        user = JSON.parse(request)
 		respond_to do |format|
-			if user.save
+			if user
 				format.html { redirect_to :dashboard}
 				format.json { render json: @event, status: :created, location: @event }
 			else
@@ -64,8 +75,9 @@ class EventsController < ApplicationController
 	# PUT /events/1
 	# PUT /events/1.json
 	def update
-		@event = Event.find(params[:id])
-
+		#@event = Event.find(params[:id])
+        request = RestClient.get 'https://sra-api.com/users/events/:id', {:params => {:id => params[:id]}} 
+        @event = JSON.parse(request)
 		respond_to do |format|
 			if @event.update_attributes(params[:event])
 				format.html { redirect_to @event, notice: 'Event was successfully updated.' }
@@ -80,8 +92,9 @@ class EventsController < ApplicationController
 	# DELETE /events/1
 	# DELETE /events/1.json
 	def destroy
-		@event = Event.find(params[:id])
-		@event.destroy
+		#@event = Event.find(params[:id])
+        RestClient.delete 'https://sra-api.com/users/events/:id', {:params => {:id => params[:id]}}
+		#@event.destroy
 
 		respond_to do |format|
 			format.html { redirect_to events_url }
