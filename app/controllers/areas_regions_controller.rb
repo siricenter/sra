121097b86f7f9e9f5f1ff065846f63210bff678d
andrewf2 <andrewf02@gmail.com>
2014-09-05 22:35:12 +0000

    Made area_regions rest client syntax more valid

--- app/controllers/areas_regions_controller.rb
+++ app/controllers/areas_regions_controller.rb
@@ -1,9 +1,9 @@
 class AreasRegionsController < ApplicationController
 	def create
 		#@area = Area.find(params[:area_id])
-        request = RestClient.get 'http://sra-api.com/areas/:id', {:params => {:id => params[:id]}}
+        request = RestClient.get "http://sra-api.com/areas/#{params[:id]}"
         @area = JSON.parse(request)
-        request = RestClient.get 'http://sra-api.com/region/:id', {:params => {:id => params[:id]}}
+   		request = RestClient.get "http://sra-api.com/region/#{params[:id]}"
 		@region = JSON.parse(request)
 		
 		unless can? :create, :area_region
@@ -22,10 +22,10 @@ class AreasRegionsController < ApplicationController
 	end
 
 	def destroy
-		request = RestClient.get 'http://sra-api.com/areas/:id', {:params => {:id => params[:id]}}
+        request = RestClient.get "http://sra-api.com/areas/#{params[:id]}"
 		#@region = Region.find(params[:region_id])
 		@area = JSON.parse(request)
-        request = RestClient.get 'http://sra-api.com/region/:id', {:params => {:id => params[:id]}}
+		request = RestClient.get "http://sra-api.com/region/#{params[:id]}" 
 		@region = JSON.parse(request)
 		unless can? :delete, :area_region
 			redirect_to @region, alert: "You don't have rights to assign areas"
@@ -33,7 +33,7 @@ class AreasRegionsController < ApplicationController
 			@region.areas.delete(@area)
 
 			respond_to do |format|
-				if @region.save!
+                if response.status == 200
 					format.html { redirect_to @region, notice: "#{@area.name} was successfully unassigned from #{@region.name}" }
 				else
 					format.html {redirect_to @region, alert: "Assignment failed"}
