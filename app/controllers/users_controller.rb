25ea74bb03bb9ae115b5f3d5aa2b50221e1b9faa
andrewf2 <andrewf02@gmail.com>
2014-09-23 15:57:20 +0000

    Cleaned the user controller to compensate for the nesting changes made on the api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,17 +13,12 @@ class UsersController < ApplicationController
 		#@user = User.find(params[:id])
         request = RestClient.get 'https://sra-api.com/users/:id',{:params => {:id => params[:id]}} 
         @user = JSON.parse(request)
-        request = RestClient.get 'https://sra-api.com/roles' {:accept => :json}
-		#@roles = Role.all
-        @roles = JSON.parse(request)
 	end
 
 	def destroy
 		#@user = User.find(params[:id])
-        request = RestClient.get 'https://sra-api.com/users/:id',{:params => {:id => params[:id]}} 
-        @user = JSON.parse(request)
         RestClient.delete 'https://sra-api.com/users/:id',{:params => {:id => params[:id]}} 
-        if @user = nil
+		if response.status = 200
 			redirect_to users_path, notice: "User ##{params[:id]} was successfully removed"
 		else
 			redirect_to users_path, notice: "User wasn't destroyed"
@@ -33,10 +28,7 @@ class UsersController < ApplicationController
 	def update_password
 		#@user = User.find(current_user.id)
         RestClient.put 'https://sra-api.com/users/:id', {:params => {:password => params[:user][:password]}}
-        request = RestClient.get 'https://sra-api/user', {:params => {:id => params[:id]}}
-        @user = JSON.parse(request)
-		if @user.update(user_params)
-			sign_in @user, :bypass => true
+		if response.status == 200
 			redirect_to root_path
 		else
 			render "edit"
@@ -45,10 +37,8 @@ class UsersController < ApplicationController
 
 	def create
         request = RestClient.post 'https://sra-api/users', {:params => {:email => params[:user][:email], :password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation]}}
-        @user = RestClient.get 'https://sra-api/user', {:params => {:id => params[:id]}}
-        @user = JSON.parse(@user)
         #@user = User.new({:email => params[:user][:email], :password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation]})
-        if @user.id == params[:user][:id]
+		if response.status == 200
 			flash[:notice] = "#{@user.email} successfully created."
 			redirect_to dashboard_path
 		else
