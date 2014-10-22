a8dcec5d41ef7c70f3a9ac4f607a3037da953aca
Cody Poll <CJPoll@gmail.com>
2014-10-21 20:40:01 -0600

    Oops - forgot to add the person spec last commit

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -19,41 +19,27 @@
 require 'spec_helper'
 
 describe Person do
-	before :each do
-		@person = FactoryGirl.build :person
-	end
-	
-	it "has a valid factory" do
-		@person.should be_valid
-	end
-	
-	it "is valid when the person is not in school" do
-		@person.in_school = "0"
-		@person.should be_valid
-	end
-
-	it "is invalid without a birthday" do
-		@person.birthday = nil
-		@person.should_not be_valid
+	before :each do 
+		@json = {
+			'person' => {
+				'id' => 1,
+				'given_name' => 'Vim',
+				'family_name' => 'Wizard',
+				'birthday' => '2014-10-21',
+				'education_level' => 'some college',
+				'gender' => 'male',
+				'in_school' => true,
+			}}
 	end
 
-	it "is invalid without an education_level" do
-		@person.education_level = nil
-		@person.should_not be_valid
-	end
-
-	it "is invalid without an family_name" do
-		@person.family_name = nil
-		@person.should_not be_valid
-	end
-
-	it "is invalid without an gender" do
-		@person.gender = nil
-		@person.should_not be_valid
-	end
-
-	it "is invalid without a given_name" do
-		@person.given_name = nil
-		@person.should_not be_valid
+	it 'creates a new person from a person json structure' do
+		person = Person.new @json
+		expect(person.id).to eq(@json['person']['id'])
+		expect(person.given_name).to eq(@json['person']['given_name'])
+		expect(person.family_name).to eq(@json['person']['family_name'])
+		expect(person.birthday).to eq(@json['person']['birthday'])
+		expect(person.education_level).to eq(@json['person']['education_level'])
+		expect(person.gender).to eq(@json['person']['gender'])
+		expect(person.in_school).to eq(@json['person']['in_school'])
 	end
 end
