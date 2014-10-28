3c2cc2e63c30b4ba58f905ce93490e3ff1c3829f
andrewf2 <andrewf02@gmail.com>
2014-10-28 22:20:39 +0000

    added check login to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
     
 
-	def after_sign_in_path_for(resource)
-        dashboard_path
+	def check_login
+		redirect_to root_path unless session[:token]
 	end
 end
