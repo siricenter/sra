6a1c874e4ae5656ed61f47e2344a14b273585895
andrewf2 <andrewf02@gmail.com>
2014-11-04 22:34:24 +0000

    area_region controller had some syntax issues

--- app/controllers/areas_regions_controller.rb
+++ app/controllers/areas_regions_controller.rb
@@ -1,10 +1,9 @@
 class AreasRegionsController < ApplicationController
 	def create
 		@area = Area.find(params[:area_id])
-        #request = RestClient.get "http://sra-api.herokuapp.com/areas/#{params[:id]}"
-        #@area = JSON.parse(request)
-   		request = RestClient.get "http://sra-api.herokuapp.com/region/#{params[:id]}"
-		@region = JSON.parse(request)
+        
+		@region = Region.find(params[:id])
+   		
 		
 		unless can? :create, :area_region
 			redirect_to @region, alert: "You don't have rights to assign areas"
@@ -22,11 +21,11 @@ class AreasRegionsController < ApplicationController
 	end
 
 	def destroy
-        request = RestClient.get "http://sra-api.herokuapp.com/areas/#{params[:id]}"
-		#@region = Region.find(params[:region_id])
-		@area = JSON.parse(request)
-		request = RestClient.get "http://sra-api.herokuapp.com/region/#{params[:id]}" 
-		@region = JSON.parse(request)
+		
+       
+		@region = Region.find(params[:region_id])
+		
+
 		unless can? :delete, :area_region
 			redirect_to @region, alert: "You don't have rights to assign areas"
 		else
